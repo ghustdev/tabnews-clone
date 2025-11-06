c1e4f8f523f979721e35fb16dd4240eb72e084f7
ghustdev <gustavocardoso0019@gmail.com>
2025-11-06 14:59:45 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-//  Componente React inicial
+//          Componente React inicial
 function Home() {
-    return <h1>Vou honrar meu pai e mãe, e vou construir um futuro próspero para nós dois!!!!! eu te amo! ❤️❤️❤️❤️</h1>
+    return <h1>Servidor no ar!</h1>
 }
 
-//  Exportr função Home para renderizar página
+//          Exportar função Home para renderizar página
 export default Home;
