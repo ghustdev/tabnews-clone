d2bd0cb1d487ffd8ad88ac653a6e7655b61e2ee2
ghustdev <gustavocardoso0019@gmail.com>
2026-02-20 09:18:28 -0300

    Feat: add 'api/v1/status'endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,8 @@
 // Apenas testes de get
 test('GET to /api/v1/status should return 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
+
   // uso do TDD para visualizar o response
   expect(response.status).toBe(200);
-  console.log(response.status);
+  console.log(response);
 });
